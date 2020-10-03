8e9cd7233bf0fe0c2eda1133e033b8e9ffe91f0d
mrifkiw <rifki.widadi29@gmail.com>
2020-10-04 05:02:27 +0700

    kedua kalinya ini commit

---
+++ src/views/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="profile">
+    <h1>This is an Profile page</h1>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+export default {
+  name: "profile",
+  components: {},
+};
+</script>
